1ac2f5127ba0e86cabbddd8603620203dbe564d6
Kraktoos <kraktoos@gmail.com>
2022-09-05 16:35:06 +0100

    added a start game menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,9 +3,13 @@ let grid_container;
 let add_buttons;
 let last_play;
 const game_container = document.querySelector("#game-container");
+const create_game = document.querySelector("#create-game");
 let relative_elements;
 
 function startGame(players, max_points, max_sets) {
+  create_game.innerHTML = ``;
+  create_game.style.display = "none";
+
   game_container.innerHTML = `
   <button id="swap-button">
     <iconify-icon icon="fluent:arrow-swap-24-regular"></iconify-icon>
@@ -50,19 +54,23 @@ function checkWinner(max_points, max_sets) {
   const red_sets_count = document.querySelector("#red-sets-count");
   const blue_sets_count = document.querySelector("#blue-sets-count");
 
-  if (red_btn.innerHTML >= max_points && red_btn.innerHTML - blue_btn.innerHTML >= 2) {
+  console.log(red_btn.innerHTML, blue_btn.innerHTML);
+
+  if (parseInt(red_btn.innerHTML) >= max_points && parseInt(red_btn.innerHTML) - parseInt(blue_btn.innerHTML) >= 2) {
     red_sets_count.innerHTML = parseInt(red_sets_count.innerHTML) + 1;
     red_btn.innerHTML = 0;
     blue_btn.innerHTML = 0;
     if (red_sets_count.innerHTML == max_sets) {
       alert("Red player won!");
+      resetGame();
     }
-  } else if (blue_btn.innerHTML >= max_points && blue_btn.innerHTML - red_btn.innerHTML >= 2) {
+  } else if (parseInt(blue_btn.innerHTML) >= max_points && parseInt(blue_btn.innerHTML) - parseInt(red_btn.innerHTML) >= 2) {
     blue_sets_count.innerHTML = parseInt(blue_sets_count.innerHTML) + 1;
     red_btn.innerHTML = 0;
     blue_btn.innerHTML = 0;
     if (blue_sets_count.innerHTML == max_sets) {
       alert("Blue player won!");
+      resetGame();
     }
   }
 }
@@ -96,17 +104,28 @@ swapElements = (max_points, max_sets) => {
   }
 };
 
+function resetGame() {
+  game_container.innerHTML = ``;
+  create_game.style.display = "flex";
+
+  create_game.innerHTML = `
+  <div class="name-picker">
+    <input type="text" id="player1-name" value="Player 1" />
+    <input type="text" id="player2-name" value="Player 2" />
+    <input type="number" id="max-points" value=11 />
+    <input type="number" id="max-sets" value=3 />
+    <button id="start-game">Start</button>
+  </div>
+  `;
+  const startGameButton = document.querySelector("#start-game");
+  startGameButton.addEventListener("click", () => {
+    const players = [{ name: document.querySelector("#player1-name").value }, { name: document.querySelector("#player2-name").value }];
+    const max_points = document.querySelector("#max-points").value;
+    const max_sets = document.querySelector("#max-sets").value;
+    startGame(players, max_points, max_sets);
+  });
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
-  startGame(
-    [
-      {
-        name: "Player 1",
-      },
-      {
-        name: "Player 2",
-      },
-    ],
-    11,
-    3
-  );
+  resetGame();
 });
